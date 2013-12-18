fb3325c0a1b3b732393f774358c204d1e2e858e7
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-18 12:46:44 +0100

    Bump to 1.2.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(  name='algoliasearch',
-        version='1.1.6',
+        version='1.2.1',
         description='Algolia Search API Client for Python',
         url='https://github.com/algolia/algoliasearch-client-python',
         packages = ["algoliasearch"],
         install_requires = ['urllib3'],
-        keywords = ['algolia', 'pyalgolia', 'search', 'backend'],
+        keywords = ['algolia', 'pyalgolia', 'search', 'backend', 'hosted', 'cloud', 'full-text search', 'faceted search'],
         classifiers = [
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
