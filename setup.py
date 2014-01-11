dfa824ecbb8e2f4e83f776d88e96043bb7a9233e
Sylvain UTARD <sylvain.utard@gmail.com>
2014-01-11 11:13:42 -0800

    Bump to 1.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(  name='algoliasearch',
-        version='1.2.3',
+        version='1.2.4',
         description='Algolia Search API Client for Python',
         url='https://github.com/algolia/algoliasearch-client-python',
         packages = ["algoliasearch"],
