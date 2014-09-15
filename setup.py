1ce62b1d158d9526556a56eaad9457b129e1a072
Sylvain UTARD <sylvain.utard@gmail.com>
2014-09-15 18:09:12 +0200

    Embed resources/ca-bundle.crt using the MANIFEST.IN file

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(  name='algoliasearch',
         description='Algolia Search API Client for Python',
         url='https://github.com/algolia/algoliasearch-client-python',
         packages = ["algoliasearch"],
-        data_files = [('resources', 'resources/ca-bundle.crt')],
         install_requires = ['urllib3 >= 1.8.2'],
         keywords = ['algolia', 'pyalgolia', 'search', 'backend', 'hosted', 'cloud', 'full-text search', 'faceted search'],
         classifiers = [
