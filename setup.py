be888647104505c75bee22ed7d7fd8a1b2115e13
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-06-18 16:15:19 +0200

    improve distribution setup

--- setup.py
+++ setup.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-import os
-import sys
 
-if sys.version < '3':
-    execfile(os.path.join('algoliasearch', 'version.py'))
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+path_version = os.path.join(os.path.dirname(__file__), 'algoliasearch/version.py')
+if sys.version_info[0] == 3:
+    execfile(path_version)
 else:
-    exec(open("algoliasearch/version.py").read())
+    exec(open(path_version).read())
 
-setup(  name='algoliasearch',
-        version=VERSION,
-        description='Algolia Search API Client for Python',
-        url='https://github.com/algolia/algoliasearch-client-python',
-        packages = ["algoliasearch"],
-        package_dir={'algoliasearch': 'algoliasearch'},
-        package_data={'algoliasearch': ['resources/*.crt']},
-        include_package_data = True,
-        install_requires = ['urllib3 >= 1.8.2'],
-        keywords = ['algolia', 'pyalgolia', 'search', 'backend', 'hosted', 'cloud', 'full-text search', 'faceted search'],
-        classifiers = [
+
+setup(
+    name = 'algoliasearch',
+    version = VERSION,
+    license = 'MIT License',
+    packages = ['algoliasearch'],
+    package_dir = {'algoliasearch': 'algoliasearch'},
+    packages_data = {'algoliasearch': ['ressources/*.crt']},
+    include_package_data = True,
+    install_requires = ['urllib3 >= 1.8.2'],
+    description = 'Algolia Search API Client for Python',
+    long_description = README,
+    author = 'Algolia Team',
+    author_email = 'hey@algolia.com',
+    url = 'https://github.com/algolia/algoliasearch-client-python',
+    keywords = ['algolia', 'pyalgolia', 'search', 'backend', 'hosted', 'cloud',
+        'full-text search', 'faceted search'],
+    classifiers = [
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-     )
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
