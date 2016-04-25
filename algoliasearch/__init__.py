74f07511d810447f8c357aadebb810f6cb67ef55
Léo Ercolanelli <leo.ercolanelli@gmail.com>
2016-04-25 17:04:34 +0200

    Make VERSION easier to access
    Until now `algoliasearch.version.VERSION` was needed to obtain the
    current version. Only `algoliasearch.VERSION` is now needed.
    The change is backward compatible: it is still possible to do
    `algoliasearch.version.VERSION`.

--- algoliasearch/__init__.py
+++ algoliasearch/__init__.py
@@ -25,10 +25,13 @@ THE SOFTWARE.
 from . import client
 from . import index
 from . import helpers
+from . import version
 
 
 # Compatibility with old import
 class algoliasearch(object):
+    VERSION = version.VERSION
+
     Client = client.Client
     Index = index.Index
     AlgoliaException = helpers.AlgoliaException
