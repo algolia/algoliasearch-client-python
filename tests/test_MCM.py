ec5e05fd3caf975d51c8a42517a5407b46acfbc5
Xavier Grand <grand.xavier@gmail.com>
2017-10-26 16:19:38 +0200

    test(MCM): fix python2 compat

--- tests/test_MCM.py
+++ tests/test_MCM.py
@@ -12,7 +12,7 @@ except ImportError:
     import unittest
 
 from algoliasearch.client import Client, MAX_API_KEY_LENGTH, RequestOptions
-from algoliasearch.helpers import AlgoliaException
+from algoliasearch.helpers import AlgoliaException,PY2
 
 from .helpers import safe_index_name
 from .helpers import get_api_client
@@ -38,6 +38,10 @@ class MCMTest(unittest.TestCase):
             apiproxy_stub_map.apiproxy.RegisterStub('urlfetch',  urlfetch_stub.URLFetchServiceStub())
         cls.client = Client(os.environ['ALGOLIA_APPLICATION_ID_MCM'],
                   os.environ['ALGOLIA_API_KEY_MCM'])
+        if PY2:
+            cls.strType = unicode
+        else:
+            cls.strType = str
 
     def test_1_list_clusters(self):
         answer = self.client.list_clusters()
@@ -45,7 +49,7 @@ class MCMTest(unittest.TestCase):
         self.assertTrue(isinstance(answer, dict))
         self.assertTrue(isinstance(answer['clusters'], list))
         self.assertTrue(len(answer['clusters']) > 0)
-        self.assertTrue(isinstance(answer['clusters'][0]['clusterName'], str))
+        self.assertTrue(isinstance(answer['clusters'][0]['clusterName'], self.strType))
         self.assertTrue(isinstance(answer['clusters'][0]['nbRecords'], int))
         self.assertTrue(isinstance(answer['clusters'][0]['nbUserIDs'], int))
         self.assertTrue(isinstance(answer['clusters'][0]['dataSize'], int))
@@ -55,7 +59,7 @@ class MCMTest(unittest.TestCase):
         answer = self.client.assign_user_id(self.uniq_user_id('python-client'), clusterName)
 
         self.assertTrue(isinstance(answer, dict))
-        self.assertTrue(isinstance(answer['createdAt'], str))
+        self.assertTrue(isinstance(answer['createdAt'], self.strType))
         time.sleep(2) # Sleep to let the cluster publish the change
 
     def test_3_list_user_ids(self):
@@ -65,8 +69,8 @@ class MCMTest(unittest.TestCase):
         self.assertTrue(isinstance(answer, dict))
         self.assertTrue(isinstance(answer['userIDs'], list))
         self.assertTrue(len(answer['userIDs']) > 0)
-        self.assertTrue(isinstance(answer['userIDs'][0]['userID'], str))
-        self.assertTrue(isinstance(answer['userIDs'][0]['clusterName'], str))
+        self.assertTrue(isinstance(answer['userIDs'][0]['userID'], self.strType))
+        self.assertTrue(isinstance(answer['userIDs'][0]['clusterName'], self.strType))
         self.assertTrue(isinstance(answer['userIDs'][0]['nbRecords'], int))
         self.assertTrue(isinstance(answer['userIDs'][0]['dataSize'], int))
 
@@ -78,7 +82,7 @@ class MCMTest(unittest.TestCase):
         self.assertTrue(isinstance(answer['topUsers'], dict))
         self.assertTrue(len(answer['topUsers']) > 0)
         self.assertTrue(isinstance(answer['topUsers'][clusterName], list))
-        self.assertTrue(isinstance(answer['topUsers'][clusterName][0]['userID'], str))
+        self.assertTrue(isinstance(answer['topUsers'][clusterName][0]['userID'], self.strType))
         self.assertTrue(isinstance(answer['topUsers'][clusterName][0]['nbRecords'], int))
         self.assertTrue(isinstance(answer['topUsers'][clusterName][0]['dataSize'], int))
 
@@ -86,8 +90,8 @@ class MCMTest(unittest.TestCase):
         answer = self.client.get_user_id(self.uniq_user_id('python-client'))
 
         self.assertTrue(isinstance(answer, dict))
-        self.assertTrue(isinstance(answer['userID'], str))
-        self.assertTrue(isinstance(answer['clusterName'], str))
+        self.assertTrue(isinstance(answer['userID'], self.strType))
+        self.assertTrue(isinstance(answer['clusterName'], self.strType))
         self.assertTrue(isinstance(answer['nbRecords'], int))
         self.assertTrue(isinstance(answer['dataSize'], int))
 
@@ -102,8 +106,8 @@ class MCMTest(unittest.TestCase):
         self.assertTrue(isinstance(answer['hitsPerPage'], int))
         self.assertTrue(isinstance(answer['hits'], list))
         self.assertTrue(len(answer['hits']) > 0)
-        self.assertTrue(isinstance(answer['hits'][0]['userID'], str))
-        self.assertTrue(isinstance(answer['hits'][0]['clusterName'], str))
+        self.assertTrue(isinstance(answer['hits'][0]['userID'], self.strType))
+        self.assertTrue(isinstance(answer['hits'][0]['clusterName'], self.strType))
         self.assertTrue(isinstance(answer['hits'][0]['nbRecords'], int))
         self.assertTrue(isinstance(answer['hits'][0]['dataSize'], int))
 
@@ -112,7 +116,7 @@ class MCMTest(unittest.TestCase):
 
         print(answer)
         self.assertTrue(isinstance(answer, dict))
-        self.assertTrue(isinstance(answer['deletedAt'], str))
+        self.assertTrue(isinstance(answer['deletedAt'], self.strType))
 
 
 
