fedc6a62d14e0cfb2e7f5d6812887401347c3e42
Julien Bourdeau <julien@sigerr.org>
2018-04-11 10:51:22 +0200

    fix(keys): allow all params to be passed in obj

--- algoliasearch/client.py
+++ algoliasearch/client.py
@@ -451,11 +451,13 @@ class Client(object):
         if not isinstance(obj, dict):
             obj = {'acl': obj}
 
-        obj.update({
-            'validity': validity,
-            'maxQueriesPerIPPerHour': max_queries_per_ip_per_hour,
-            'maxHitsPerQuery': max_hits_per_query
-        })
+        # Check with `is not None`, because 0 is evaluated to False
+        if validity is not None:
+            obj['validity'] = validity
+        if max_queries_per_ip_per_hour is not None:
+            obj['maxQueriesPerIPPerHour'] = max_queries_per_ip_per_hour
+        if max_hits_per_query is not None:
+            obj['maxHitsPerQuery'] = max_hits_per_query
 
         if indexes:
             obj['indexes'] = indexes
