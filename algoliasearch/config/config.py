f9e1b301dbda1c1475e586cc5b5b9113f23180ea
Nuno Maduro <enunomaduro@gmail.com>
2019-01-03 17:30:01 +0100

    Refactors config

--- algoliasearch/config/config.py
+++ algoliasearch/config/config.py
@@ -1,28 +1,9 @@
-from algoliasearch.http.hosts import Hosts
-from algoliasearch.http.host import Host
-
-
 class Config(object):
     def __init__(self, app_id, api_key):
         # type: (str, str) -> None
 
         self.app_id = app_id
         self.api_key = api_key
-        self.hosts = self.__create_from_app_id(app_id)
         self.read_timeout = 5
         self.write_timeout = 5
         self.connect_timeout = 5
-
-    def __create_from_app_id(self, app_id):
-        # type: (str) -> Hosts
-
-        read_hosts = write_hosts = [
-            Host('%s-1.algolianet.com' % app_id),
-            Host('%s-2.algolianet.com' % app_id),
-            Host('%s-3.algolianet.com' % app_id)
-        ]
-
-        read_hosts.append(Host('%s-dsn.algolia.net' % app_id, 10))
-        write_hosts.append(Host('%s.algolia.net' % app_id, 10))
-
-        return Hosts(read_hosts, write_hosts)
--- algoliasearch/config/search_config.py
+++ algoliasearch/config/search_config.py
@@ -1,5 +1,22 @@
 from algoliasearch.config.config import Config
+from algoliasearch.http.host import Host
+from algoliasearch.http.hosts import Hosts
 
 
 class SearchConfig(Config):
-    pass
+
+    def __init__(self, app_id, api_key):
+        # type: (str, str) -> None
+
+        Config.__init__(self, app_id, api_key)
+
+        read_hosts = write_hosts = [
+            Host('%s-1.algolianet.com' % app_id),
+            Host('%s-2.algolianet.com' % app_id),
+            Host('%s-3.algolianet.com' % app_id)
+        ]
+
+        read_hosts.append(Host('%s-dsn.algolia.net' % app_id, 10))
+        write_hosts.append(Host('%s.algolia.net' % app_id, 10))
+
+        self.hosts = Hosts(read_hosts, write_hosts)
