cbcbec183122f6946a83cacfbe9406d1ef2d3949
Nuno Maduro <enunomaduro@gmail.com>
2019-01-04 17:57:51 +0100

    Tests dict access in response

---
+++ tests/unit/response/test_response.py
@@ -0,0 +1,18 @@
+import unittest
+
+from algoliasearch.response.response import Response
+
+
+class TestResponse(unittest.TestCase):
+    def test_dict_access(self):
+        response = {
+            'foo': 'bar',
+        }
+
+        response = FooResponse({}, response)
+        self.assertEqual(response['foo'], 'bar')
+
+
+class FooResponse(Response):
+    def wait(self):
+        pass
