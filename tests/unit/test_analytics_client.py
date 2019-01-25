1299bcb98b1077c6101ed1ec62c981cb90c5820d
Nuno Maduro <enunomaduro@gmail.com>
2019-01-25 10:34:35 +0100

    Tests default region

---
+++ tests/unit/test_analytics_client.py
@@ -0,0 +1,15 @@
+import unittest
+
+from algoliasearch.analytics_client import AnalyticsClient
+
+
+class TestAnalyticsClient(unittest.TestCase):
+    def test_create(self):
+        client = AnalyticsClient.create('foo', 'bar')
+
+        self.assertIsInstance(client, AnalyticsClient)
+
+    def test_default_region(self):
+        client = AnalyticsClient.create('foo', 'bar')
+
+        self.assertEqual(client._AnalyticsClient__Config.region, 'us')
