96e9362ac5f285db28cb13a7d0b720fa949af807
Nuno Maduro <enunomaduro@gmail.com>
2019-03-13 16:25:30 +0100

    Adds coverage test

--- tests/run_tests_coverage.sh
+++ tests/run_tests_coverage.sh
@@ -5,12 +5,12 @@ coverage erase
 # Runs non sync tests
 export TEST_TYPE=snyc
 pip uninstall asyncio aiohttp async_timeout
-coverage run --source algoliasearch ./../setup.py test
+coverage run --source algoliasearch ./setup.py test
 
 # Runs async tests
 export TEST_TYPE=async
 pip install 'asyncio>=3.4,<4.0' 'aiohttp>=2.0,<4.0' 'async_timeout>=2.0,<4.0'
-coverage run -a --source algoliasearch ./../setup.py test
+coverage run -a --source algoliasearch ./setup.py test
 
 # Tests coverage
 coverage report --fail-under=99
