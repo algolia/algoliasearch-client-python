5fab1c189cbb977fa265c6f9dd15b4aafa09197d
Anthony Seure <anthony.seure@gmail.com>
2020-02-28 13:25:54 +0100

    test: ensure AB testing tests do not fail if indices are not created yet

--- tests/helpers/misc.py
+++ tests/helpers/misc.py
@@ -5,26 +5,22 @@ from algoliasearch.exceptions import RequestException
 
 class RetryableClient:
 
-    def __init__(self, base):
-
+    def __init__(self, base, retryable_messages):
         self._base = base
+        self.retryable_messages = retryable_messages
 
     def __getattr__(self, name):
-
         method = getattr(self._base, name)
 
         def closure(*args, **kwargs):
             result = None
-
             while result is None:
-
                 try:
                     result = method(*args, **kwargs)
                 except RequestException as e:
-                    if str(e) != 'Too Many Requests':
+                    if str(e) not in self.retryable_messages:
                         raise e
                     time.sleep(1)
-
             return result
 
         return closure
