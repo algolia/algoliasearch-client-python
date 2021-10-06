f06c7437e68acaeba42b3a0d5bf6bdc26a9e6b87
Clément Vannicatte <vannicattec@gmail.com>
2021-10-06 18:14:55 +0200

    feat: better testing strategy

--- tests/features/test_recommend_client.py
+++ tests/features/test_recommend_client.py
@@ -1,112 +1,67 @@
-from algoliasearch.http.transporter import Request
+import json
+import requests
 from algoliasearch.recommend_client import RecommendClient
 import unittest
 import mock
 
+# we ignore the `line-too-long` lint errors for the file
+# flake8: noqa E501
+
 
 class TestRecommendClient(unittest.TestCase):
-    host = "1/indexes/*/recommendations?"
-
     def setUp(self):
         self.client = RecommendClient.create("foo", "bar")
+        # `_session` is initialized with `None` to make it closable, we override it
+        # here to mock it.
+        self.client._transporter._requester._session = requests.Session()
+        self.client._transporter._requester._session.send = mock.Mock(name="send")
 
     def tearDown(self):
         self.client.close()
 
-    def build_request(self, requests):
-        req = Request(
-            "POST",
-            {
-                "X-Algolia-Application-Id": "foo",
-                "X-Algolia-API-Key": "bar",
-                "User-Agent": mock.ANY,
-                "Content-Type": "application/json",
-            },
-            {"requests": requests},
-            2,
-            5,
-            {},
-        )
+    def assert_requests(self, expected):
+        for i, raw_req in enumerate(
+            self.client._transporter._requester._session.send.call_args_list
+        ):
+            # req is in `call -> PreparedRequest`
+            req = raw_req[0][0]
 
-        return req
+            self.assertEqual(req.method, expected[i]["method"])
+            self.assertEqual(req.path_url, expected[i]["path"])
+            self.assertDictEqual(json.loads(req.body), json.loads(expected[i]["body"]))
 
     def test_get_recommendations(self):
         # Test method with 'bought-together' model
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_recommendations(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                    "model": "bought-together",
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        try:
+            self.client.get_recommendations(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
                         "model": "bought-together",
-                        "threshold": 0,
-                    }
+                    },
                 ]
-            ),
-            self.host,
-        )
+            )
+        except Exception:
+            pass
 
         # Test method with 'related-products' mode
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_recommendations(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                    "model": "related-products",
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        try:
+            self.client.get_recommendations(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
                         "model": "related-products",
-                        "threshold": 0,
-                    }
+                    },
                 ]
-            ),
-            self.host,
-        )
+            )
+        except Exception:
+            pass
 
         # Test method with multiple requests and specified thresholds
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_recommendations(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE-1",
-                    "model": "related-products",
-                    "threshold": 0,
-                },
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE-2",
-                    "model": "related-products",
-                    "threshold": 0,
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        try:
+            self.client.get_recommendations(
                 [
                     {
                         "indexName": "products",
@@ -121,195 +76,170 @@ class TestRecommendClient(unittest.TestCase):
                         "threshold": 0,
                     },
                 ]
-            ),
-            self.host,
-        )
+            )
+        except Exception:
+            pass
 
         # Test overrides undefined threshold with default value
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_recommendations(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                    "model": "related-products",
-                    "threshold": None,
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        try:
+            self.client.get_recommendations(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
                         "model": "related-products",
-                        "threshold": 0,
-                    }
+                        "threshold": None,
+                    },
                 ]
-            ),
-            self.host,
-        )
+            )
+        except Exception:
+            pass
 
         # Test threshold is overriden by specified value
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_recommendations(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                    "model": "related-products",
-                    "threshold": 42,
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        try:
+            self.client.get_recommendations(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
                         "model": "related-products",
                         "threshold": 42,
-                    }
+                    },
                 ]
-            ),
-            self.host,
+            )
+        except Exception:
+            pass
+
+        self.assert_requests(
+            [
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"bought-together","threshold":0}]}',
+                },
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"related-products","threshold":0}]}',
+                },
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE-1","model":"related-products","threshold":0},{"indexName":"products","objectID":"B018APC4LE-2","model":"related-products","threshold":0}]}',
+                },
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"related-products","threshold":0}]}',
+                },
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"related-products","threshold":42}]}',
+                },
+            ]
         )
 
     def test_get_related_products(self):
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_related_products(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        # Test method
+        try:
+            self.client.get_related_products(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
-                        "model": "related-products",
-                        "threshold": 0,
-                    }
+                    },
                 ]
-            ),
-            self.host,
-        )
+            )
+        except Exception:
+            pass
 
         # Test if it overrides wrong given model
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_related_products(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                    "model": "bought-together",
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        try:
+            self.client.get_related_products(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
-                        "model": "related-products",
-                        "threshold": 0,
-                    }
+                        "model": "bought-together",
+                    },
                 ]
-            ),
-            self.host,
+            )
+        except Exception:
+            pass
+
+        self.assert_requests(
+            [
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"related-products","threshold":0}]}',
+                },
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"related-products","threshold":0}]}',
+                },
+            ]
         )
 
     def test_get_frequently_bought_together(self):
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_frequently_bought_together(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        # Test method
+        try:
+            self.client.get_frequently_bought_together(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
-                        "model": "bought-together",
-                        "threshold": 0,
-                    }
+                    },
                 ]
-            ),
-            self.host,
-        )
+            )
+        except Exception:
+            pass
 
         # Test if it overrides wrong given model
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_frequently_bought_together(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                    "model": "related-products",
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        try:
+            self.client.get_frequently_bought_together(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
-                        "model": "bought-together",
-                        "threshold": 0,
-                    }
+                    },
                 ]
-            ),
-            self.host,
-        )
+            )
+        except Exception:
+            pass
 
-        # Check if `fallbackParameters` param is not passed for 'bought-together' method
-        self.client._transporter.retry = mock.Mock(name="retry")
-        self.client.get_frequently_bought_together(
-            [
-                {
-                    "indexName": "products",
-                    "objectID": "B018APC4LE",
-                    "fallbackParameters": {"facetFilters": []},
-                },
-            ]
-        )
-        self.client._transporter.retry.assert_called_with(
-            # we don't check the hosts
-            mock.ANY,
-            self.build_request(
+        # Test if `fallbackParameters` param is not passed for 'bought-together' method
+        try:
+            self.client.get_frequently_bought_together(
                 [
                     {
                         "indexName": "products",
                         "objectID": "B018APC4LE",
-                        "model": "bought-together",
-                        "threshold": 0,
-                    }
+                        "fallbackParameters": {"facetFilters": []},
+                    },
                 ]
-            ),
-            self.host,
+            )
+        except Exception:
+            pass
+
+        self.assert_requests(
+            [
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"bought-together","threshold":0}]}',
+                },
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"bought-together","threshold":0}]}',
+                },
+                {
+                    "method": "POST",
+                    "path": "/1/indexes/*/recommendations",
+                    "body": '{"requests":[{"indexName":"products","objectID":"B018APC4LE","model":"bought-together","threshold":0}]}',
+                },
+            ]
         )
