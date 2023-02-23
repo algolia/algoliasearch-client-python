7d3bb161398b890267b9c178c72d8f06a1db36cd
shortcuts <vannicattec@gmail.com>
2023-02-23 10:50:26 +0100

    fix: recommendation client

--- algoliasearch/recommendation_client_async.py
+++ algoliasearch/recommendation_client_async.py
@@ -22,22 +22,19 @@ class RecommendationClientAsync(RecommendationClient):
 
         _create_async_methods_in(self, client)
 
-    @asyncio.coroutine
-    def __aenter__(self):
+    async def __aenter__(self):
         # type: () -> RecommendationClientAsync # type: ignore
 
         return self  # type: ignore
 
-    @asyncio.coroutine
-    def __aexit__(self, exc_type, exc, tb):  # type: ignore
+    async def __aexit__(self, exc_type, exc, tb):  # type: ignore
         # type: (Optional[Type[BaseException]], Optional[BaseException],Optional[types.TracebackType]) -> None # noqa: E501
 
-        yield from self.close_async()  # type: ignore
+        await self.close_async()  # type: ignore
 
-    @asyncio.coroutine
-    def close_async(self):  # type: ignore
+    async def close_async(self):  # type: ignore
         # type: () -> None
 
         super().close()
 
-        yield from self._transporter_async.close()  # type: ignore
+        await self._transporter_async.close()  # type: ignore
