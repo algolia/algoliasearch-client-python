42bd5065f1818e72be43c49a7bd53d8d96893de1
Mehmet Ali Gok <33124154+mehmetaligok@users.noreply.github.com>
2023-05-09 14:55:19 +0200

    feat(go): Add context support to all requests (#1527)
    ## 🧭 What and Why
    
    🎟 JIRA Ticket: https://algolia.atlassian.net/browse/APIC-696
    
    We need to provide a way to accept users' context while respecting general conventions in Go. 
    
    ### Changes included:
    
    - Wrapper functions with a `WithContext` suffix added for all API methods to comply with the general DX standards. 
    Ex:
    ```
    
    // CreateAuthentication wraps CreateAuthenticationWithContext using context.Background.
    func (c *APIClient) CreateAuthentication(r ApiCreateAuthenticationRequest, opts ...Option) (*AuthenticationCreateResponse, error) {
    	return c.CreateAuthenticationWithContext(context.Background(), r, opts...)
    }
    
    // @return AuthenticationCreateResponse
    func (c *APIClient) CreateAuthenticationWithContext(ctx context.Context, r ApiCreateAuthenticationRequest, opts ...Option) (*AuthenticationCreateResponse, error) {
    .
    .
    .
    // actual implementation
    }
    ```
    - Playground example added to show context usage. 
    
    ## 🧪 Test
    Code generation and CI pipeline should be enough.

--- playground/go/personalization.go
+++ playground/go/personalization.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/algolia/algoliasearch-client-go/v4/algolia/personalization"
 )
 
 func testPersonalization(appID, apiKey string) int {
 	personalizationClient := personalization.NewClient(appID, apiKey, personalization.US)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
 
-	deleteUserProfileResponse, err := personalizationClient.DeleteUserProfile(
+	// it will fail expectedly because of the very short timeout to showcase the context usage.
+	deleteUserProfileResponse, err := personalizationClient.DeleteUserProfileWithContext(ctx,
 		personalizationClient.NewApiDeleteUserProfileRequest("userToken"),
 	)
 	if err != nil {
