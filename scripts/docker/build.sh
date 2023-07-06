ba9f26ab0f787b1258cf1e047ed86832f0a66641
Clément Vannicatte <vannicattec@gmail.com>
2023-07-06 10:22:17 +0200

    chore: forward dart version (#1695)

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -8,10 +8,12 @@ JAVA_VERSION=$(cat config/.java-version)
 NODE_VERSION=$(cat .nvmrc)
 PHP_VERSION=$(cat config/.php-version)
 GO_VERSION=$(cat config/.go-version)
+DART_VERSION=$(cat config/.dart-version)
 
 docker build \
   --build-arg JAVA_VERSION=$JAVA_VERSION \
   --build-arg NODE_VERSION=$NODE_VERSION \
   --build-arg PHP_VERSION=$PHP_VERSION \
   --build-arg GO_VERSION=$GO_VERSION \
+  --build-arg DART_VERSION=$DART_VERSION \
   -t api-clients-automation .
